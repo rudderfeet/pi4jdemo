eff9c7bffc29f5b5eac7ba02f7f34194a03360e3
Scott McCrory <scott@mccrory.us>
2018-08-31 18:47:23 -0400

    Initial import

---
+++ src/main/java/us/mccrory/pi4jdemo/Pi4jdemoApplication.java
@@ -0,0 +1,12 @@
+package us.mccrory.pi4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi4jdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pi4jdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/us/mccrory/pi4jdemo/Pi4jdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package us.mccrory.pi4jdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pi4jdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
